2c80d50bb868bfc8bcd75cd4aa8de269bb725121
Long Phu <PhuHL@sendo.vn>
2018-01-29 17:24:49 +0700

    Travis Add Service Mysql & mongodb

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"time"
+	"net/url"
+	"database/sql"
+
 	log "github.com/sirupsen/logrus"
 	"github.com/longphu-thesis/go-gin-app/utils"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
@@ -15,6 +20,26 @@ func init() {
 
 func main() {
 	defer utils.TimeTrack(time.Now(), "Run Main")
+
+	dbUser := "travis"
+	dbPass := ""
+	dbHost := "locahost"
+	dbPort := "3306"
+	dbName := "golang_test"
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+
+	dbConn, err := sql.Open(`mysql`, dsn)
+	if err != nil {
+		log.Error(err)
+	} else {
+		log.Info("Connected Mysql")
+	}
+	defer dbConn.Close()
 
 	// go vet ./...
 	str := "hello world!"
