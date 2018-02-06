3b0014a00015ea143048b22b9b164680c8ad5240
Long Phu <PhuHL@sendo.vn>
2018-02-06 17:37:35 +0700

    Fix Config File

--- example/example.go
+++ example/example.go
@@ -1,23 +1,42 @@
 package example
 
 import (
+
 	"fmt"
+	"strings"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/longphu-thesis/go-gin-app/example/redis"
 	"github.com/longphu-thesis/go-gin-app/example/stringutil"
 
-	cfg "github.com/longphu-thesis/go-gin-app/config/env"
+	"github.com/spf13/viper"
 )
 
-var config cfg.Config
-
 func init() {
-	config = cfg.NewViperConfig()
+	viper.SetEnvPrefix(`go_gin_app`)
+	viper.AutomaticEnv()
 
-	if config.GetBool(`debug`) {
+	replacer := strings.NewReplacer(`.`, `_`)
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetConfigType(`yaml`)
+	//viper.AddConfigPath(`./config/`)
+	viper.SetConfigFile(`config.yml`)
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	if viper.GetBool(`debug`) {
 		log.Info(fmt.Sprintf("Service RUN on DEBUG mode"))
 	}
+
+	//os.Setenv("GO_GIN_APP_MYSQL_HOST", "localhost")
+
+	//log.Info(viper.AllKeys())
+	//log.Info(viper.ConfigFileUsed())
+	//log.Info(viper.Get(`mysql.host`))
 }
 
 func Run() {
